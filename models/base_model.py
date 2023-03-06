e0304b805419dcf954165394fb94bc5a14400b7d
Julius-Okeyo <jaykopiyo@gmail.com>
2023-03-06 04:04:42 -0800

    Created new branch and started working on the BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+class BaseModel
+defines all common attributes/methods for other classes
+'''
+import datetime
+class BaseModel:
+    '''
+    BaseModel with attributes and methods inherited by other classes
+    '''
+    def __init__(self):
+        '''
+        creates new instance of the BaseModel
+        '''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+    def __str__(self):
+        '''
+        defines how the returned string is printed out in stdout
+        '''
+        print([<class name>] (<self.id>) <self.__dict__>)
