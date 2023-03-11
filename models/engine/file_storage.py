ad37272e6335c3ac288ac54afc5ee1658d140553
Julius-Okeyo <jaykopiyo@gmail.com>
2023-03-11 11:34:35 -0800

    Started working on file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,12 @@
 '''
 FileStorage
 '''
+import json
+from models.base_model import BaseModel
 class FileStorage(BaseModel):
     '''
     Defines class FileStorage for conducting JSON dump and JSON load
     '''
-    __file_path
-    __objects
+    __file_path = "file.json"
+    __objects = {}
 
