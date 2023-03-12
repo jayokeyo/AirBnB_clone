7d18f47c2d17d1a00fea07b4c62b9e1f09751cd7
Julius-Okeyo <jaykopiyo@gmail.com>
2023-03-12 01:17:36 -0800

    Added class user

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Class User
+'''
+class User(BaseModel):
+    '''
+    Defines class User with attributes that describe the application user
+    '''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
