61b840e586998da61c5f1564573c8eb4e815b6d3
Julius-Okeyo <jaykopiyo@gmail.com>
2023-03-18 03:56:40 -0700

    Merged branches

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+class State
+'''
+from models.base_model import BaseModel
+class State(BaseModel):
+    '''
+    Defines class State with public class attribute state name
+    '''
+    name = ""
+
